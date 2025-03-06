6ab774494307091a3018cb731210be384de49277
Jorge Eduardo Vicente Hernández <jorge.eduardo@vicenteh.com>
2025-03-06 16:22:17 -0500

    Update sett.py

--- app/0_utility/sett.py
+++ app/0_utility/sett.py
@@ -1,1 +1,37 @@
+# -*- coding: utf-8 -*-
 
+#################################
+# Rutas
+#################################
+
+path_app = "/content/drive/My Drive/Colab Notebooks/Iniciativas/20240429_Unir/Tesis"
+path_model = path_app + "/" + "1_model"
+
+
+#################################
+# Keys
+#################################
+
+apikey_openai = "sk-"
+api_key_time_series = ""
+
+
+#################################
+# Models
+#################################
+
+classification_model = "lightgbm_model_classification.pkl"
+classification_label = "lightgbm_model_classification_label_encoder.pkl"
+
+sentimental_model = "gpt-3.5-turbo"
+
+conversational_model = "gpt-3.5-turbo"
+orchestrating_agent_model = "gpt-3.5-turbo"
+functional_agent_model = "gpt-3.5-turbo"
+
+
+#################################
+# Chatbot configuration
+#################################
+
+chatbot_name = "Forexin"
